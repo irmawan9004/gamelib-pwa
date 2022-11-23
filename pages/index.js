b020e335517cab6968c856d79df3830eacdf74e5
irmawan <irmawan9001@gmail.com>
2022-11-23 21:01:48 +0700

    fix deploy problem

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 import Meta from "../components/Meta";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
