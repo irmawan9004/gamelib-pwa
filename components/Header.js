611a5346f1d6d1e7f99886fffe9cd9f0c4b0e7f0
irmawan <irmawan9001@gmail.com>
2023-11-20 04:16:24 +0700

    rplbk TA

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ import controller from "../public/assets/controller.webp";
 export default function Header() {
   return (
     <header class="text-gray-600 body-font bg-gray-900">
-      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <div class="container mx-auto flex justify-between flex-wrap p-5 flex-col md:flex-row items-center">
         <Link
           href="/"
           class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
@@ -15,6 +15,11 @@ export default function Header() {
             <span class="ml-3 text-xl text-white font-extrabold">GAMELIB</span>
             <p class="ml-3  text-sm text-slate-200">Be Number One Gamers</p>
           </div>
+        </Link>
+        <Link href="/login">
+          <button class="bg-purple-700 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded-full">
+            Login
+          </button>
         </Link>
       </div>
     </header>
---
+++ pages/login.js
@@ -0,0 +1,118 @@
+import { useState } from "react";
+import Image from "next/image";
+import logo from "../public/assets/apple-icon-precomposed.png";
+import axios from "axios";
+import Link from "next/link";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+
+  const Auth = async (e) => {
+    e.preventDefault();
+    try {
+      await axios
+        .post("http://localhost:8000/api/login", {
+          email: email,
+          password: password,
+        })
+        .then((res) => {
+          localStorage.setItem("token", res.data.accessToken);
+        });
+      alert("Login Success");
+      window.location.href = "/";
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.msg);
+      }
+    }
+  };
+
+  return (
+    <section class="bg-gradient-to-r from-purple-300 to-purple-900 dark:bg-gray-900">
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <Link
+          href="/"
+          class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <Image class="w-8 h-8 mr-2" src={logo} alt="logo" />
+          <h1 class=" text-xl font-extrabold">GAMELIB</h1>
+        </Link>
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+            <p className="text-center text-rose-600">{msg}</p>
+            <form onSubmit={Auth} class="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label
+                  for="email"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  for="password"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required=""
+                />
+              </div>
+              <div class="flex items-center justify-between">
+                <div class="flex items-start">
+                  <div class="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-purple-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-purple-600 dark:ring-offset-gray-800"
+                      required=""
+                    />
+                  </div>
+                  <div class="ml-3 text-sm">
+                    <label
+                      for="remember"
+                      class="text-gray-500 dark:text-gray-300"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <button
+                type="submit"
+                class="w-full text-white bg-purple-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Sign in
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
